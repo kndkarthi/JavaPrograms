e24df0f292745e5846fcc2b2d64be5d836db6d47
Terry Schmidt <terryschmidt@gmail.com>
2014-05-02 16:34:51 -0500

    Create DelimPos.java

---
+++ DelimPos.java
@@ -0,0 +1,16 @@
+package hw3solutions;
+
+import java.util.Stack;
+
+//Terry Schmidt
+//CSC402
+
+public class DelimPos {
+	int linecnt;
+	char ch;
+	
+	public DelimPos (int l, char c) {
+		linecnt = l;
+		ch = c;
+	}
+}
